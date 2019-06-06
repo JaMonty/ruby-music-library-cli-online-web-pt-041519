5f24c6625f0ef3624d1ff0e32007705d000a91a3
Jason  Montes <jasone.montes@gmail.com>
2019-06-06 01:21:20 +0000

    Automatically backed up by Learn

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,26 @@
+class MusicLibraryController
+
+  def initialize(path = "./db/mp3s")
+    MusicImporter.new(path).import
+  end
+
+  def call
+    input = ""
+
+    puts ("Welcome to your music library!")
+
+    input = gets.strip
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+end
